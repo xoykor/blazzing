ffb56d0992748a8b6871407fbab480daf34d2c1a
xoykor <138058470+xoykor@users.noreply.github.com>
2026-09-08 14:03:08 -0300

    feat(ui): add streaming hub entry point

---
+++ include/visual_iptv/hub.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef VISUAL_IPTV_HUB_H
+#define VISUAL_IPTV_HUB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int vip_hub_run(void);
+int vip_pluto_app_run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
